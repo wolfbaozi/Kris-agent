107f6c3a0d1ea64f569759df1afb921237e2734c
ex_wuzr13 <ex_wuzr13@partner.midea.com>
2026-06-09 14:07:35 +0800

    feat: 移除 SkillModal.vue 中的 computed 导入，简化代码结构

--- web/src/components/SkillModal.vue
+++ web/src/components/SkillModal.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted, computed } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useSkillStore } from '../stores/skill'
 import { useAuthStore } from '../stores/auth'
 
